b79633aa4ff10e1c4b1c2d76c0222317c5d9262c
Wisarut41372 <mos0880040156@gmail.com>
2021-11-16 17:25:29 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Ng101';
+ user = {id: 1001 , 
+  name:"Wisarut K.",
+  address:"Thailand"}
+
+  i:number = 0;
+  nameInput:string="";
+
+  sayHi(){
+    return "Hi from" + this.user.name;
+  }
+  addCount(){
+    this.i++;
+  }
+  delCount(){
+    this.i--;
+  }
+  readName(name:string){
+    console.log(name)
+    this.nameInput = name;
+  }
+  valid: boolean = 1>2;
+  validAge: boolean = false;
+
+  readAge(Age: string){
+    let a = parseInt(Age)
+    this.validAge = a>=18 && a<=25
+  }
+  menus =["Shabu","KFC","MK","BBQ","SALAD"];
+  courses=[
+    {id: 1, title:"TypeScript"},
+    {id: 2, title:"java"},
+    {id: 3, title:"php"},
+    {id: 4, title:"html"},
+    {id: 5, title:"python"},
+  ];
+  
 }
